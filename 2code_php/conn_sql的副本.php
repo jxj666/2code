9c16205e163e22213432a54bc2364e464a932cda
jxj322991 <228322991@qq.com>
2018-12-21 14:54:55 +0800

    fix

--- 2code_php/conn_sql的副本.php
+++ 2code_php/conn_sql的副本.php
@@ -1,7 +1,7 @@
 <?php 
 
 
-$conn = mysqli_connect('sqld-gz.bsdfcehost.com'.':'.'3306','f4aa0cbadf4f8b4ddb94bfdf61732a41ddf2','c270db7bd7dffb41988a11d809c9f51958','LUsLJCdsfcZwmemZdfMlfmwfnVQ');
-// $conn = mysqli_connect('路径'.':'.'端口','账号','密码','库名')
+$conn = mysqli_connect('sql234host.com'.':'.'3306','f4aa0cbadf4f8b4ddb342194bfdf61732a41ddf2','c270db7bd7dffb41988a11234d809c9f51958','LUsLJCdsfc234ZwmemZdfMlfmwfnVQ');
+
 
 ?>
