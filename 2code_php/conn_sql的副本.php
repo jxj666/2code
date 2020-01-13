366c7f49d33ded438bec3d0b500be073a2bee9a6
jxj322991 <228322991@qq.com>
2020-01-13 16:27:48 +0800

    fix

--- 2code_php/conn_sql的副本.php
+++ 2code_php/conn_sql的副本.php
@@ -1,7 +1,12 @@
 <?php 
-
-
-$conn = mysqli_connect('sql234host.com'.':'.'3306','f4aa0cbadf4f8b4ddb342194bfdf61732a41ddf2','c270db7bd7dffb41988a11234d809c9f51958','LUsLJCdsfc234ZwmemZdfMlfmwfnVQ');
-
-
+$conn = mysqli_connect('host','username','password','dbname');
 ?>
+
+<!-- 连接你的数据库 -->
+<!-- mysqli_connect(host,username,password,dbname,port,socket); -->
+<!-- host	可选。规定主机名或 IP 地址。
+username	可选。规定 MySQL 用户名。
+password	可选。规定 MySQL 密码。
+dbname	可选。规定默认使用的数据库。
+port	可选。规定尝试连接到 MySQL 服务器的端口号。
+socket	可选。规定 socket 或要使用的已命名 pipe。 -->
